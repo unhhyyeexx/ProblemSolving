7a358bded9cabed0b1029c35e744c71ffccfe97f
정아현 <unhhyyee66@gmail.com>
2023-03-28 21:12:33 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,15 @@
+def solution(storey):
+    answer = 0
+    while storey:
+        rmd = storey%10
+        if rmd > 5:
+            answer += (10-rmd)
+            storey += 10
+        elif rmd < 5:
+            answer += rmd
+        else:
+            if (storey//10) % 10 > 4:
+                storey += 10
+            answer += rmd
+        storey //= 10
+    return answer
