7a0a51cb12850eb428f1b9fc193edf9f71ed56ad
정아현 <unhhyyee66@gmail.com>
2023-06-14 22:10:26 +0900

    [level 3] Title: 풍선 터트리기, Time: 529.45 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -0,0 +1,26 @@
+def solution(a):
+    n = len(a)
+    if n == 1:
+        return 1
+    
+    answer = 2
+    minLeft = [a[0]]
+    minRight = [a[-1]]
+    
+    for i in range(1, n):
+        if a[i] < minLeft[-1]:
+            minLeft.append(a[i])
+        else:
+            minLeft.append(minLeft[-1])
+        
+        if a[n-1-i] < minRight[-1]:
+            minRight.append(a[n-1-i])
+        else:
+            minRight.append(minRight[-1])
+    minRight.reverse()
+    
+    for i in range(1, n-1):
+        if minLeft[i-1] > a[i] or minRight[i+1]>a[i]:
+            answer += 1
+    
+    return answer
