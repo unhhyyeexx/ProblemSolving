4dd05cfd78aeee261b3a6206d63f59681d6c6ee7
unhhyyeexx <unhhyyee66@gmail.com>
2024-02-29 15:50:49 +0900

    [Gold II] Title: 저울, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -1,17 +1,17 @@
+# boj 2437 저울 gold2
 
 import sys
 input = sys.stdin.readline
 
-def solution(weights):
+def solution(ws):
     now = 1
-    weights.sort()
     for i in range(n):
-        if now< weights[i]:
+        if now < ws[i]:
             break
-        now += weights[i]
+        now += ws[i]
 
     return now
 
 n = int(input())
-weights = list(map(int, input().split()))
-print(solution(weights))
+ws = list(map(int, input().split()))
+print(solution(sorted(ws)))
