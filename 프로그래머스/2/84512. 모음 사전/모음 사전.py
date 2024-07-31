39f0b8ed57f188c3cce042a6cf70c78b61908dc5
unhhyyeexx <unhhyyee66@gmail.com>
2024-07-31 13:17:21 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,32 +1,12 @@
-# from itertools import product
-
-# def solution(word):
-#     total = []
-#     for i in range(1, 6):
-#         for j in product(['A','E','I','O','U'], repeat=i):
-#             total.append(''.join(list(j)))
-    
-#     total.sort()
-#     return total.index(word) + 1
-
-# def solution(word):
-#     answer = 0
-#     dic = ['A','E','I','O','U']
-#     li = [5**i for i in range(len(dic))]
-#     for i in range(len(word)-1, -1, -1):
-#         idx = dic.index(word[i])
-#         for j in range(5-i):
-#             answer += li[j]*idx
-#         answer += 1
-#     return answer
 
 def solution(word):
-    dic = {'A': 1, 'E': 2, 'I': 3, 'O': 4, 'U': 5}
+    answer = 0
+    dic = {'A':0, 'E':1, 'I':2, 'O':3, 'U':4}
     n = len(word)
     answer = n
     for i in range(n):
         tmp = 0
         for j in range(4-i, -1, -1):
-            tmp += 5 ** j
-        answer += tmp * (dic[word[i]] -1)
+            tmp += 5**j
+        answer += tmp * dic[word[i]]
     return answer
