0f22e46243ea786b217fa4fcbb749f00a908eff2
unhhyyeexx <unhhyyee66@gmail.com>
2024-10-18 13:37:04 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.38 ms
+메모리: 10.3 MB, 시간: 0.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 23:01:49
+2024년 10월 18일 13:37:00
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,21 @@
-def dfs(i, n, computers, visit):
-    visit[i] = 1
-    for j in range(n):
-        if visit[j] == 0 and computers[i][j] == 1:
-            dfs(j, n, computers, visit)
 
+def dfs(n, computers, now, idx, graph):
+    graph[now] = idx
+    for i in range(n):
+        if graph[i] == 0 and computers[now][i] == 1:
+            graph[i] = idx
+            dfs(n, computers, i, idx, graph)
+    return
 
 def solution(n, computers):
     answer = 0
-                
-    visit = [0] * n
-    for i in range(n):
-        if visit[i] == 0:
-            dfs(i, n, computers, visit)
-            answer += 1
+    graph = [0] * (n+1)
+    idx = 1
+    for gi in range(n):
+        if graph[gi] == 0:
+            dfs(n, computers, gi, idx, graph)
+            idx += 1
+    
+    answer = max(graph)
         
     return answer
