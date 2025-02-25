db889f2aef9a6f18797e6b220ed56253c3607bc4
unhhyyeexx <unhhyyee66@gmail.com>
2025-02-25 20:37:51 +0900

    [Gold III] Title: 트리 복구, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Gold/6597. 트리 복구/트리 복구.js
+++ 백준/Gold/6597. 트리 복구/트리 복구.js
@@ -2,24 +2,28 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 function solution() {
+  let answer;
+  let preorder;
+  let inorder;
+  let idx;
     
-    function postorder(order) {
-        if (order === '') return;
-        const temp = preorder[idx];
-        const [left, right] = order.split(preorder[idx]);
-        idx += 1;
-        postorder(left);
-        postorder(right);
-        res += temp;
-    }
-    
-    for (let i=0; i<input.length-1; i++) {
-        var [preorder, inorder] = input[i].split(' ');
-        var idx = 0;
-        var res = '';
-        postorder(inorder);
-        console.log(res);
-    }
+  function postorder(order) {
+      if (order === '') return;
+      const temp = preorder[idx];
+      const [left, right] = order.split(preorder[idx]);
+      idx += 1;
+      postorder(left);
+      postorder(right);
+      res += temp;
+  }
+  
+  for (let i=0; i<input.length-1; i++) {
+      [preorder, inorder] = input[i].split(' ');
+      idx = 0;
+      res = '';
+      postorder(inorder);
+      console.log(res);
+  }
 }
 
 solution();
