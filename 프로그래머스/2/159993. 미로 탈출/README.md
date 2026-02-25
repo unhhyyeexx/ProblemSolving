f31717993b96a18b829085c942946c5554a060a8
unhhyyeexx <unhhyyee66@gmail.com>
2026-02-25 14:41:52 +0900

    [level 2] Title: 미로 탈출, Time: 14.33 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 19.17 ms
+메모리: 9.34 MB, 시간: 14.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 23:29:53
+2026년 02월 25일 14:41:50
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,52 +1,43 @@
 from collections import deque
 
+def bfs(start, end, maps, n, m):
+    visit = [[0] * m for _ in range(n)]
+    q = deque()
+    q.append(start)
+    dirs = [[0,1], [0,-1], [1,0], [-1,0]]
+    while len(q) > 0:
+        ci, cj = q.popleft()
+        cv = visit[ci][cj]
+        for di, dj in dirs:
+            ni, nj = ci+di, cj+dj
+            if (0<=ni<n and 0<=nj<m):
+                if (maps[ni][nj] == "X"): continue
+                if (ni==end[0] and nj==end[1]):
+                    return cv+1
+                else:
+                    if visit[ni][nj] == 0:
+                        visit[ni][nj] = cv + 1
+                        q.append([ni, nj])
+            
 def solution(maps):
-    answer = 0
     n, m = len(maps), len(maps[0])
+    start, end, lever = [], [], []
     
-    s, l = [], []
+    # find start point
     for i in range(n):
+        if (start and end and lever): break
         for j in range(m):
-            if maps[i][j] == 'S': s = [i, j]
-            elif maps[i][j] == 'L': l = [i, j]
+            if maps[i][j] == 'S':
+                start = [i, j]
+            elif maps[i][j] == 'E':
+                end = [i, j]
+            elif maps[i][j] == 'L':
+                lever = [i, j]
+                
+    stol = bfs(start, lever, maps, n, m)
+    ltoe = bfs(lever, end, maps, n, m)
     
-    dir = [[0,1], [0,-1], [1,0], [-1,0]]
-    # 시작부터 레버까지
-    visit = [[0]*m for _ in range(n)]
-    q = deque()
-    q.append(s)
-    visit[s[0]][s[1]] = 1
-    while q:
-        i, j = q.popleft()
-        lcheck = 0
-        for d in dir:
-            ni, nj = i+d[0], j+d[1]
-            if 0<=ni<n and 0<=nj<m:
-                if maps[ni][nj] == 'L':
-                    answer += visit[i][j]
-                    lcheck = 1
-                    break
-                if maps[ni][nj] in ['O', 'E'] and visit[ni][nj] == 0:
-                    visit[ni][nj] = visit[i][j] + 1
-                    q.append([ni, nj])
-        if lcheck: 
-            break
-    if answer == 0:
+    if not stol or not ltoe:
         return -1
-    
-    # 레버부터 출구까지
-    q = deque()
-    q.append(l)
-    visit = [[0]*m for _ in range(n)]
-    visit[l[0]][l[1]] = 1
-    while q:
-        i, j = q.popleft()
-        for d in dir:
-            ni, nj = i+d[0], j+d[1]
-            if 0<=ni<n and 0<=nj<m:
-                if maps[ni][nj] == 'E':
-                    return answer + visit[i][j]
-                if maps[ni][nj] in ['S', 'O'] and visit[ni][nj] == 0:
-                    q.append([ni, nj])
-                    visit[ni][nj] = visit[i][j] + 1
-    return -1
+    else:
+        return stol + ltoe
