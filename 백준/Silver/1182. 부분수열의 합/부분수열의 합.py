31c1d58a4ce447b2c68a9ed04ddf1047c584d27c
unhhyyeexx <unhhyyee66@gmail.com>
2026-03-14 17:42:32 +0900

    [Silver II] Title: 부분수열의 합, Time: 152 ms, Memory: 112528 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,26 +1,26 @@
-
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-def solution(idx, sum, n, s, nums):
+
+def dfs(idx, sum):
     global answer
-    if idx >= n:
+
+    if idx == n:
+        if sum == s:
+            answer += 1
         return
     
-    sum += nums[idx]
-
-    if sum == s:
-        answer += 1
-
-    #현재수를 선택
-    solution(idx+1, sum, n, s, nums)
-    #현재 수 선택 안함
-    solution(idx+1, sum-nums[idx], n, s, nums)
-
+    dfs(idx+1, sum)
+    dfs(idx+1, sum+nums[idx])
+    
 
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
 answer = 0
-solution(0, 0, n, s, nums)
+dfs(0,0)
 
+if s == 0:
+    answer -= 1
+    
 print(answer)
