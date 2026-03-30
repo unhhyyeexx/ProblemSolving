0f746e57d6d653dde630419691a32e9eddaa9aef
unhhyyeexx <unhhyyee66@gmail.com>
2026-03-30 19:29:30 +0900

    [Gold III] Title: 내리막 길, Time: 124 ms, Memory: 45016 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,35 @@
-# dp, dfs
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-dir = [[0,1], [0,-1], [1,0], [-1,0]]
-def solution(n, m, board, dp, i, j):
-    if i == n-1 and j == m-1:
+m, n = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(m)]
+
+# dp[x][y] = (x,y)에서 목적지까지 가는 경로 수
+dp = [[-1]*n for _ in range(m)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def dfs(x, y):
+    # 목적지 도착
+    if x == m-1 and y == n-1:
         return 1
 
-    if dp[i][j] == -1:
-        dp[i][j] = 0
+    # 이미 계산된 경우
+    if dp[x][y] != -1:
+        return dp[x][y]
 
-        for di, dj in dir:
-            ni = i + di
-            nj = j + dj
-            if 0<=ni<n and 0<=nj<m:
-                if board[ni][nj] < board[i][j]:
-                    dp[i][j] += solution(n, m, board, dp, ni, nj)
+    dp[x][y] = 0
 
-    return dp[i][j]
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-n, m = map(int, input().split())
-board = []
-dp = [[-1]*m for _ in range(n)]
-for _ in range(n):
-    line = list(map(int, input().split()))
-    board.append(line)
+        if 0 <= nx < m and 0 <= ny < n:
+            if board[nx][ny] < board[x][y]:
+                dp[x][y] += dfs(nx, ny)
 
-print(solution(n, m, board, dp, 0, 0))
+    return dp[x][y]
+
+print(dfs(0,0))
