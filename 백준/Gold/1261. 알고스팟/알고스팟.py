86388e99b75b2b832ae6aaf64ab5d06a60dbdbc8
unhhyyeexx <unhhyyee66@gmail.com>
2026-04-02 16:55:50 +0900

    [Gold IV] Title: 알고스팟, Time: 148 ms, Memory: 112628 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,32 +1,31 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
+import heapq
 
 m, n = map(int, input().split(" "))
 board = [list(input().strip()) for _ in range(n)]
 
-visit = [[float('inf')]*m for _ in range(n)]
+dist = [[float("inf")] * m for _ in range(n)]
+pq = []
 
-q = deque()
-visit[0][0] = 0
-q.append((0,0))
+dist[0][0] = 0
+heapq.heappush(pq, (0,0,0)) # cost, x, y
 
 dirs = [[0,1], [0,-1], [1,0], [-1,0]]
-while len(q) > 0:
-    i, j = q.popleft()
+while len(pq) > 0:
+    cost, i, j= heapq.heappop(pq)
+    
+    if cost > dist[i][j]:
+        continue
+
     for di, dj in dirs:
-        ni = i + di
-        nj = j + dj
+        ni = i+di
+        nj = j+dj
         if 0<=ni<n and 0<=nj<m:
+            new_cost = cost + int(board[ni][nj])
 
-            cost = visit[i][j] + int(board[ni][nj])
+            if new_cost < dist[ni][nj]:
+                dist[ni][nj] = new_cost
+                heapq.heappush(pq, (new_cost, ni, nj))
 
-            if cost < visit[ni][nj]:
-                visit[ni][nj] = cost
-
-                if board[ni][nj] == '0':
-                    q.appendleft((ni, nj))
-                else:
-                    q.append((ni, nj))
-
-print(visit[n-1][m-1])
+print(dist[n-1][m-1])
