d7932a6dd83bd7b571947eb74d7eed56b00bca12
unhhyyeexx <unhhyyee66@gmail.com>
2026-04-03 15:36:41 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 340 ms, Memory: 50664 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,42 +1,32 @@
-# boj 1197 최소 스패닝 트리 gold4
-# 최소신장트리(mst) => Kruskal & Prim 이용.
-# Kruskal은 edge(간선)이 적은 문제에 적합. Prim은 많은 문제에 적합
-
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-def solution(edges, parent):
-    answer = 0
-    def get_parent(x):
-        if parent[x] == x:
-            return x
-        parent[x] = get_parent(parent[x])
-        return parent[x]
-    
-    def union_parent(a, b):
-        a = get_parent(a)
-        b = get_parent(b)
-        if a < b:
-            parent[b] = a
-        else:
-            parent[a] = b
-        
-    def check_parent(a, b):
-        return get_parent(a) == get_parent(b)
-
-    edges.sort(key=lambda x:x[2])
-
-    for a, b, c in edges:
-        if not check_parent(a, b):
-            union_parent(a, b)
-            answer += c
-
-    return answer
-
-v, e = map(int, input().split())
-edge = []
+v, e = map(int, input().split(" "))
+edges = []
 for _ in range(e):
-    edge.append(list(map(int, input().split())))
-parenttt = [i for i in range(v+1)]
+    a, b, c = map(int, input().split(" "))
+    edges.append((c, a, b))
 
-print(solution(edge, parenttt))
+edges.sort()
+
+parent = [i for i in range(v+1)]
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x, y):
+    rx = find(x)
+    ry = find(y)
+
+    if rx != ry:
+        parent[ry] = rx
+
+answer = 0
+for cost, a, b in edges:
+    if find(a) != find(b):
+        union(a, b)
+        answer += cost
+    
+print(answer)
