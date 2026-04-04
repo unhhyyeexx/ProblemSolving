b374e6ce01ee0d8ddbb7752c68e4509e899b443b
unhhyyeexx <unhhyyee66@gmail.com>
2026-04-04 14:38:41 +0900

    [Gold V] Title: 최소비용 구하기, Time: 272 ms, Memory: 58232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,33 +1,34 @@
-import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+import heapq
+INF = float("inf")
 
 n = int(input())
 m = int(input())
-graph = [[] for i in range(n+1)]
-distance = [INF] * (n+1)
-
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    s, g, c = map(int, input().split())
-    graph[s].append((g, c))
-
-start, end = map(int, input().split())
+    a, b, c= map(int, input().split(" "))
+    graph[a].append((c, b))
+start, arrive = map(int, input().split(" "))
 
 def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
+    distance = [INF] * (n+1)
+    pq = []
     distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+    heapq.heappush(pq, (0, start))
+
+    while pq:
+        dist, now = heapq.heappop(pq)
+
+        if dist > distance[now]:
             continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
 
-dijkstra(start)
+        for cost, next in graph[now]:
+            new_cost = dist + cost
+            if new_cost < distance[next]:
+                distance[next] = new_cost
+                heapq.heappush(pq, (new_cost, next))
+    
+    return distance[arrive]
 
-print(distance[end])
+print(dijkstra(start))
