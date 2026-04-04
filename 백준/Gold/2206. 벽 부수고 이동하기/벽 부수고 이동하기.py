ea003e254955a9442f2f89dc848b05adc7727288
unhhyyeexx <unhhyyee66@gmail.com>
2026-04-04 17:15:10 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 5720 ms, Memory: 151452 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,29 +1,34 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
 
-dir = [[0,1],[0,-1],[1,0],[-1,0]]
-def solution(i, j, state):
-    visit = [[[0]*2 for _ in range(m)] for _ in range(n)]
-    visit[0][0][0] = 1
+n, m = map(int, input().split(" "))
+board = [list(input().rstrip()) for _ in range(n)]
 
-    q = deque([[i, j, state]])
+visit = [[[False]*2 for _ in range(m)] for _ in range(n)]
+q = deque()
+visit[0][0][0] = True
+q.append((0,0,0,0)) # r, c, 경로길이, 벽부쉈는지안부쉈는지
 
-    while q:
-        i, j, state = q.popleft()
-        if i == n-1 and j == m-1:
-            return visit[i][j][state]
-        for d in dir:
-            ni, nj = i+d[0], j+d[1]
-            if ni<0 or nj<0 or ni>=n or nj>=m:
-                continue
-            if maps[ni][nj] == 1 and state == 0:
-                visit[ni][nj][1] = visit[i][j][state] + 1
-                q.append([ni, nj, 1])
-            elif maps[ni][nj] == 0 and visit[ni][nj][state] == 0:
-                visit[ni][nj][state] = visit[i][j][state] + 1
-                q.append([ni,nj,state])
-    return -1
+dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+while q:
+    i, j, dist, wall = q.popleft()
 
+    if i == n-1 and j == m-1:
+        print(dist + 1)
+        exit()
+    
+    for di, dj in dirs:
+        ni, nj = i + di, j + dj
+        if 0<=ni<n and 0<=nj<m:
+            # 벽 있어서 부숴볼게
+            if not wall and board[ni][nj] == '1' and visit[ni][nj][1] == False:
+                visit[ni][nj][1] = True
+                q.append((ni, nj, dist+1, True))
+            # 벽 없네
+            if board[ni][nj] == '0' and visit[ni][nj][wall] == False:
+                visit[ni][nj][wall] = True
+                q.append((ni, nj, dist+1, wall))
+    
 
-n, m = map(int, input().split())
-maps = [list(map(int, input())) for _ in range(n)]
-print(solution(0,0,0))
+print(-1)
