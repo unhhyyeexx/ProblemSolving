a29650c02cfbd067602743c0abc83260809c937a
unhhyyeexx <unhhyyee66@gmail.com>
2026-04-04 13:10:43 +0900

    [Gold V] Title: 로봇 청소기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,45 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-# 인덱스 기준 직진 방향
-dir = [[-1,0],[0,1],[1,0],[0,-1]]
-def robot(r, c, d):
-    clean = 0
-    stop = 0
+n, m = map(int, input().split(" "))
+r, c, d = map(int, input().split(" "))
+dirs = [[-1,0], [0,1], [1,0], [0,-1]] # 북, 동, 남, 서
 
-    while not stop:
-        blank = 1
-        # 1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
-        if maps[r][c] == 0:
-            maps[r][c] = 2
-            clean += 1
+board = [list(map(int, input().split(" "))) for _ in range(n)]
 
-        # 3. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우,
-        for _ in range(4):
-            # 3-1. 반시계 방향으로 90도 회전한다.
-            d = (d+3)%4
-            ni, nj = r + dir[d][0], c + dir[d][1]
-            # 3-2. 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
-            if 0<=ni<N and 0<=nj<M and maps[ni][nj] == 0:
-                r, c = ni, nj
-                blank = 0
-                break    
-        
-        # 2. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우, (blank = 1)
-        if blank == 1:
-        # 2-1. 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아간다.
-            bi, bj = r-dir[d][0], c-dir[d][1]
-            if 0<=bi<N and 0<=bj<M and maps[bi][bj] != 1:
-                r, c = bi, bj
-                continue
-            # 2-2. 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
-            else:
-                stop = 1
+answer = 0
+while True:
+    # 현재칸이 청소되지 않은 경우, 현재 칸을 청소한다
+    if board[r][c] == 0:
+        board[r][c] = 2
+        answer += 1
     
-    return clean
+    # 현재 칸 주변 4칸 탐색
+    clean = False
+    for i in range(4):
+        d = (d-1+4)%4
+        ni = r + dirs[d][0]
+        nj = c + dirs[d][1]
+        # 앞에 청소되지 않은 칸이 있다면 한칸 전진 후 청소로 돌아감
+        if 0<=ni<n and 0<=nj<m and board[ni][nj] == 0:
+            clean = True
+            r = ni
+            c = nj
+            break
+    
+    # 청소되지 않은 빈칸이 없어서 로봇청소기 방향이 원복됐다면
+    # 바라보는 방향을 유지한 채 한 칸 후진 (가능하다면)
+    if not clean:
+        nd = (d+2)%4
+        ni = r + dirs[nd][0]
+        nj = c + dirs[nd][1]
+        if 0<=ni<n and 0<=nj<m and board[ni][nj] != 1:
+            r = ni
+            c = nj
+        else:
+            break
 
-
-N, M = map(int, input().split())
-R, C, D = map(int, input().split())
-maps = [list(map(int, input().split())) for _ in range(N)]
-print(robot(R, C, D))
+print(answer)
