2c3e237ed65385119d6aea82efe1a3601728d708
unhhyyeexx <unhhyyee66@gmail.com>
2026-04-04 14:55:00 +0900

    [Gold IV] Title: 플로이드, Time: 1308 ms, Memory: 44888 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+import heapq
+INF = float("inf")
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c= map(int, input().split(" "))
+    graph[a].append((c, b))
+
+def dijkstra(start):
+    distance = [INF] * (n+1)
+    pq = []
+    distance[start] = 0
+    heapq.heappush(pq, (0, start))
+
+    while pq:
+        dist, now = heapq.heappop(pq)
+
+        if dist > distance[now]:
+            continue
+
+        for cost, next in graph[now]:
+            new_cost = dist + cost
+            if new_cost < distance[next]:
+                distance[next] = new_cost
+                heapq.heappush(pq, (new_cost, next))
+    
+    return distance
+
+for i in range(1, n+1):
+    answer = []
+    for d in dijkstra(i)[1:]:
+        if d != INF:
+            answer.append(d)
+        else:
+            answer.append(0)
+    print(*answer)
